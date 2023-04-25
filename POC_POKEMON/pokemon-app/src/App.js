8b5b53dc968dabb82a46848ebaf371435c371c1c
singhneetush <singh.neetush@gmail.com>
2023-04-26 00:00:24 +0530

    (feat):implement functionality and UI

--- POC_POKEMON/pokemon-app/src/App.js
+++ POC_POKEMON/pokemon-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PokeCard from './components/content/PokeCard';
+import PokeDetails from './components/content/PokeDetails';
+import PokeTablePage from './components/content/PokeTablePage';
+import Home from './components/Home';
+import CardTest from './components/content/GetRandomPokeCard';
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' exact element={<Home />} />
+					<Route path='/poke-table' element={<PokeTablePage />} />
+					<Route path='/poke-card' element={<PokeCard />} />
+					<Route path='/poke-details-card' element={<PokeDetails />} />
+					<Route path='/poke-random' element={<CardTest />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
